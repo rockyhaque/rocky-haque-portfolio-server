0918923addf710d7a989293540091df0bd3c178e
Rocky Haque <rockyhaque99@gmail.com>
2025-08-04 11:01:47 +0600

    test: deploy

--- src/app.ts
+++ src/app.ts
@@ -23,11 +23,30 @@ app.use(cookieParser())
 //     credentials: true,
 //   })
 // )
+// app.use(
+//   cors({
+//     origin: '*',
+//   })
+// );
+
 app.use(
   cors({
-    origin: '*',
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'http://localhost:3000',
+        'https://www.rockyhaque.com',
+        'https://rocky-haque.vercel.app',
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
   })
 );
+
 
 
 app.use('/api/auth', authRouter)
